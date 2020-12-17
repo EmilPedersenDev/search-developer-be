b40c92533da3c29a84c24d178c37aad8b53b29f4
Emil Pedersen <emil.pedersen@stretch.se>
2020-12-17 14:48:44 +0100

    adding uploading image edit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,70 +1,20 @@
-const db = require("../models");
 const config = require("../config/auth.config");
-const User = db.user;
-
-const Op = db.Sequelize.Op;
-
 let jwt = require("jsonwebtoken");
 let bcrypt = require("bcryptjs");
 
-exports.signup = (req, res) => {
-  User.create({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8),
-  })
-    .then(() => {
-      res.status(200).send({
-        message: "User was created",
-      });
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message,
-      });
+exports.setToken = (reqBody, user) => {
+  let passwordIsValid = bcrypt.compareSync(reqBody.password, user.password);
+
+  if (!passwordIsValid) {
+    return res.status(401).send({
+      acessToken: null,
+      message: "Invalid Password",
     });
-};
+  }
 
-exports.signin = (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email,
-    },
-  })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send({
-          message: "User not found.",
-        });
-      }
-      let passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
+  let token = jwt.sign({ id: user.id, email: user.email }, config.secret, {
+    expiresIn: 86400, // 24 hrs
+  });
 
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          acessToken: null,
-          message: "Invalid Password",
-        });
-      }
-
-      let token = jwt.sign({ id: user.id, email: user.email }, config.secret, {
-        expiresIn: 86400, // 24 hrs
-      });
-
-      res.status(200).send({
-        id: user.id,
-        firstname: user.firstname,
-        lastname: user.lastname,
-        email: user.email,
-        accessToken: token,
-      });
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message,
-      });
-    });
+  return token;
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,7 +4,10 @@ const {
   userSigninValidation,
   userSignupValidation,
 } = require("../middleware");
-const controller = require("../controllers/auth.controller");
+const { setToken } = require("../controllers/auth.controller");
+
+const db = require("../models");
+const User = db.user;
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
@@ -20,13 +23,52 @@ module.exports = function (app) {
     userSignupValidation(),
     validate,
     [verifySignup.checkDuplicateEmail],
-    controller.signup
+    (req, res) => {
+      User.create({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8),
+      })
+        .then(() => {
+          res.status(200).send({
+            message: "User was created",
+          });
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message: err.message,
+          });
+        });
+    }
   );
 
-  app.post(
-    "/api/auth/signin",
-    userSigninValidation(),
-    validate,
-    controller.signin
-  );
+  app.post("/api/auth/signin", userSigninValidation(), validate, (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).send({
+            message: "User not found.",
+          });
+        }
+        let token = setToken(req.body, user);
+
+        res.status(200).send({
+          id: user.id,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email,
+          accessToken: token,
+        });
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message,
+        });
+      });
+  });
 };
