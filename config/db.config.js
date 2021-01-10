7b42ef1abf0bea7beb6a462de7cb26198c8f7580
Emil Pedersen <emil.pedersen@stretch.se>
2021-01-10 23:01:46 +0100

    new db config

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
 let isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-  HOST: isProduction ? "us-cdbr-east-02.cleardb.com" : "127.0.0.1",
-  USER: isProduction ? "bc90a6cc42fbe3" : "root",
-  PASSWORD: isProduction ? "a6f0b9cf" : "Searchdeveloper#10",
-  DB: isProduction ? "heroku_ec6f904e983c656" : "developerdb",
+  HOST: isProduction ? "eu-cdbr-west-03.cleardb.net" : "127.0.0.1",
+  USER: isProduction ? "bcc5a82e99b6ce" : "root",
+  PASSWORD: isProduction ? "c2f0f1e2" : "Searchdeveloper#10",
+  DB: isProduction ? "heroku_79c10e2aad2b46f" : "developerdb",
   dialect: "mysql",
   pool: {
     max: 5,
