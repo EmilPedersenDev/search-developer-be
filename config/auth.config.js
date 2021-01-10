602f99603bffbdd5f265523dc3def38fe5440e4b
Emil Pedersen <emil.pedersen@stretch.se>
2021-01-11 00:01:49 +0100

    adding env

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  secret: "search-developer-secret-key",
+  secret: process.env.SECRET,
 };
--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,8 @@
-let isProduction = process.env.NODE_ENV === "production";
-
 module.exports = {
-  HOST: isProduction ? "eu-cdbr-west-03.cleardb.net" : "127.0.0.1",
-  USER: isProduction ? "bcc5a82e99b6ce" : "root",
-  PASSWORD: isProduction ? "c2f0f1e2" : "Searchdeveloper#10",
-  DB: isProduction ? "heroku_79c10e2aad2b46f" : "developerdb",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
   dialect: "mysql",
   pool: {
     max: 5,
