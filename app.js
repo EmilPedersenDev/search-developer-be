59da5c92bb6ed59d9a44d3360cc3ff26a0ec8807
Emil Pedersen <emil.pedersen@stretch.se>
2021-01-11 00:35:22 +0100

    dsfsdf

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ debug: process.env.DEBUG });
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
